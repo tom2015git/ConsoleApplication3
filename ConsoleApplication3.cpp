cec8020c75c250f8752cc00e257627fb3880d6ee
tom2015git <tom2015git@sina.com>
2015-10-25 00:19:19 +0800

    test
    test

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication3.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout<<"hello world"<<std::endl;
+	return 0;
+}
+
